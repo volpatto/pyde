c9f2dc8fc5e8a72d6e360487ffdadfc767145e12
volpatto <volpatto@lncc.br>
2018-09-18 23:05:18 -0300

    Fix accordling to codacy code review

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -13,7 +13,7 @@ def fobj(x):
 @pytest.mark.parametrize("dim", [3, 4, 5])
 def test_ackley(dim):
 
-    sol, fobj_sol, conv_flag, log = de.optimization.optimize(
+    sol, fobj_sol, _, _ = de.optimization.optimize(
         fobj=fobj,
         dim=dim,
         low_limit=-6.0,
@@ -21,14 +21,16 @@ def test_ackley(dim):
         N=50
     )
 
-    assert sol == pytest.approx(np.zeros(dim), abs=1e-5)
-    assert fobj_sol == pytest.approx(0.0, abs=1e-5)
+    pass_conditions = (sol == pytest.approx(np.zeros(dim), abs=1e-5)) and \
+                      (fobj_sol == pytest.approx(0.0, abs=1e-5))
+    if not pass_conditions:
+        raise AssertionError()
 
 
 @pytest.mark.parametrize("dim", [2, 3, 4])
 def test_rosenbrock(dim):
 
-    sol, fobj_sol, conv_flag, log = de.optimization.optimize(
+    sol, fobj_sol, _, _ = de.optimization.optimize(
         fobj=de.benchmarks.f_rosenbrock,
         dim=dim,
         low_limit=-6.0,
@@ -36,5 +38,7 @@ def test_rosenbrock(dim):
         N=100
     )
 
-    assert sol == pytest.approx(np.ones(dim), rel=5e-2)
-    assert fobj_sol == pytest.approx(0.0, abs=1e-5)
+    pass_conditions = (sol == pytest.approx(np.ones(dim), rel=5e-2)) and \
+                     (fobj_sol == pytest.approx(0.0, abs=1e-5))
+    if not pass_conditions:
+        raise AssertionError()
